559e1fc45084045fecee2a8190e06cd7a2e015bf
Ayan pathan <pathanayan9457@gmail.com>
2024-06-11 15:02:04 +0530

    related service working fine

---
+++ public/assets/js/service-list.js
@@ -0,0 +1,39 @@
+const serviceList = {
+    "webdevelopment": {
+      "Website Development": "/service/webdevelopment/website-development",
+      "WordPress Development": "/service/webdevelopment/wordpress-development"
+    },
+    "webdesigning": {
+      "Website Redesigning": "/service/webdesigning/website-redesigning",
+      "Template Designing": "/service/webdesigning/template-designing",
+      "Landing Page Designing": "/service/webdesigning/landingpage-designing"
+    },
+    "appdevelopment": {
+      "Android App Development": "/service/appdevelopment/androidapp-development",
+      "IOS App Development": "/service/appdevelopment/ios-app-development",
+      "React Native App Development": "/service/appdevelopment/reactnative-app-development"
+    },
+    "softwaredevelopment": {
+      "Charity Management System": "/service/softwaredevelopment/charity-management-system",
+      "School Management System": "/service/softwaredevelopment/school-management-system"
+    },
+    "support": {
+      "Website Maintenance": "/service/support/website-maintenance",
+      "Mobile App Maintenance": "/service/support/mobile-app-maintenance"
+    },
+    "designing": {
+      "Website UI & UX Design": "/service/designing/web-UI-UX design",
+      "App UI & UX Design": "/service/designing/app-UI-UX design",
+      "WordPress Theme Design": "/service/designing/wordpress-theme-design"
+    },
+    "digitalmarketing": {
+      "Charity Advertising": "/service/digitalmarketing/charity-advertising",
+      "Charity Search Engine Optimisation": "/service/digitalmarketing/charity-search-engine-optimisation",
+      "Charity Social Media Optimisation": "/service/digitalmarketing/charity-social-media-optimisation",
+      "Charity Ads Remarketing": "/service/digitalmarketing/charity-ads-remarketing",
+      "Charity Content Marketing": "/service/digitalmarketing/charity-content-marketing"
+    }
+  };
+  
+  export default serviceList;
+  
